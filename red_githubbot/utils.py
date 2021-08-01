6dc1432fbc7218dc7eda6f0f005dbe625e3d1462
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-08-02 00:59:04 +0200

    Add utility for creating check runs

--- red_githubbot/utils.py
+++ red_githubbot/utils.py
@@ -1,8 +1,10 @@
 import asyncio
+import dataclasses
 import datetime
+import enum
 import os
 from collections.abc import Callable, MutableMapping
-from typing import Any, Union
+from typing import Any, Optional, Union
 
 import aiohttp
 import cachetools
@@ -23,6 +25,33 @@ machine_gh = gh_aiohttp.GitHubAPI(
 
 _gh_installation_tokens_cache = cachetools.TTLCache(maxsize=100, ttl=55 * 60)
 _P = ParamSpec("P")
+
+
+class CheckRunStatus(enum.Enum):
+    QUEUED = "queued"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+
+class CheckRunConclusion(enum.Enum):
+    ACTION_REQUIRED = "action_required"
+    CANCELLED = "cancelled"
+    FAILURE = "failure"
+    NEUTRAL = "neutral"
+    SUCCESS = "success"
+    SKIPPED = "skipped"
+    TIMED_OUT = "timed_out"
+
+
+@dataclasses.dataclass
+class CheckRunOutput:
+    title: str
+    summary: str
+    text: Optional[str] = None
+    # Output can also contain `annotations` and `images` but they can always be added in the future
+
+    def to_dict(self) -> dict[str, Any]:
+        return dataclasses.asdict(self)
 
 
 async def get_gh_client(installation_id: Union[int, str]) -> gh_aiohttp.GitHubAPI:
@@ -58,6 +87,32 @@ async def leave_comment(gh: gh_aiohttp.GitHubAPI, issue_number: int, body: str) 
     issue_comment_url = f"/repos/{UPSTREAM_REPO}/issues/{issue_number}/comments"
     data = {"body": body}
     await gh.post(issue_comment_url, data=data)
+
+
+async def post_check_run(
+    gh: gh_aiohttp.GitHubAPI,
+    *,
+    name: str,
+    head_sha: str,
+    status: Optional[CheckRunStatus] = None,
+    conclusion: Optional[CheckRunConclusion] = None,
+    details_url: Optional[str] = None,
+    output: Optional[CheckRunOutput] = None,
+) -> None:
+    check_run_url = f"/repos/{UPSTREAM_REPO}/check-runs"
+    data = {"name": name, "head_sha": head_sha}
+    if status is not None:
+        if conclusion is not None and status is not CheckRunStatus.COMPLETED:
+            raise RuntimeError("`status` needs to be `COMPLETED` when `conclusion` is provided.")
+        data["status"] = status.value
+    if conclusion is not None:
+        data["conclusion"] = conclusion.value
+    if details_url is not None:
+        data["details_url"] = details_url
+    if output is not None:
+        data["output"] = output.to_dict()
+
+    await gh.post(check_run_url)
 
 
 def add_job(func: Callable[_P, Any], *args: _P.args, **kwargs: _P.kwargs) -> Job:
